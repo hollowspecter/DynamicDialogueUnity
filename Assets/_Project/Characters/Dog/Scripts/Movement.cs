ffa2ddcec302bc3f86820bccacb7e1358ed8cb15
Vivien Baguio <baguiov@posteo.net>
2020-11-18 19:53:01 +0100

    Setup sample scene with doggo and human that follows doggo

---
+++ Assets/_Project/Characters/Dog/Scripts/Movement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+	protected const float InputSqrThreshold = 0.1f;
+
+	[Header("Settings")]
+	[SerializeField]
+	protected float horizontalVelocity = 3f;
+	[SerializeField]
+	protected float rotationSpeed = 0.1f;
+	[SerializeField]
+	protected bool disableMovement;
+	[Header("References")]
+	[SerializeField]
+	protected Animator animator;
+	[Header("Debug")]
+
+	protected Vector3 moveVector;
+	protected Vector2 input;
+	protected float inputMagnitudeSqr;
+	protected Vector3 desiredMoveDirection;
+	protected float verticalVelocity;
+
+	protected new Camera camera;
+	protected CharacterController controller;
+
+	protected virtual void Start()
+	{
+		camera = Camera.main;
+		controller = GetComponent<CharacterController>();
+	}
+
+	protected virtual void Update()
+	{
+		HandleInput();
+		MoveAndRotate();
+		HandleGravity();
+
+		if (!disableMovement)
+			controller.Move(moveVector);
+
+		UpdateAnimator();
+	}
+
+	protected virtual void MoveAndRotate()
+	{
+		inputMagnitudeSqr = input.sqrMagnitude;
+
+		if (input.sqrMagnitude > InputSqrThreshold &&
+			!disableMovement)
+		{
+			var forward = camera.transform.forward;
+			var right = camera.transform.right;
+			forward.y = 0f;
+			right.y = 0f;
+			forward.Normalize();
+			right.Normalize();
+			desiredMoveDirection = forward * input.y + right * input.x;
+
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), rotationSpeed);
+			moveVector = desiredMoveDirection * Time.deltaTime * horizontalVelocity;
+		}
+		else
+		{
+			moveVector = Vector3.zero;
+		}
+	}
+
+	protected virtual void HandleGravity()
+	{
+		if (controller.isGrounded)
+			verticalVelocity -= 0;
+		else
+			verticalVelocity -= 0.2f;
+		moveVector.y = verticalVelocity * 0.2f * Time.deltaTime;
+	}
+
+	protected virtual void UpdateAnimator()
+	{
+	}
+
+	protected virtual void HandleInput()
+	{
+		input.x = Input.GetAxis("Horizontal");
+		input.y = Input.GetAxis("Vertical");
+	}
+}
---
+++ Assets/_Project/Characters/Owner/FollowDog.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class FollowDog : MonoBehaviour
+{
+
+	[SerializeField]
+	protected Transform target;
+	[SerializeField]
+	protected float desiredRotationSpeed = 0.1f;
+
+	protected NavMeshAgent agent;
+
+	protected virtual void Awake()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	protected virtual void Update()
+	{
+		agent.SetDestination(target.position);
+	}
+
+	protected virtual void LookAt(Vector3 pos)
+	{
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(pos), desiredRotationSpeed);
+	}
+}
